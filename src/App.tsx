8c380abcae8a91c415067a7a7c3d93590d0fa24b
Matt Mazzola <mattm@microsoft.com>
2017-10-17 14:36:13 -0700

    Update app to demonstrate simple mentions plugin

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,70 @@
-import * as React from 'react';
-import './App.css';
+import * as React from 'react'
+import { EditorState } from 'draft-js'
+import Editor from 'draft-js-plugins-editor'
+import createMentionPlugin, { defaultSuggestionsFilter } from 'draft-js-mention-plugin'
+import 'draft-js-mention-plugin/lib/plugin.css'
+import './App.css'
+import mentions from './mentions'
 
-const logo = require('./logo.svg');
+export default class SimpleMentionEditor extends React.Component<{}, {}> {
+  editor: any
+  mentionPlugin: any
 
-class App extends React.Component {
+  constructor(props: any) {
+    super(props);
+
+    this.mentionPlugin = createMentionPlugin({
+      entityMutability: 'IMMUTABLE',
+      mentionPrefix: '@'
+    });
+  }
+
+  state = {
+    editorState: EditorState.createEmpty(),
+    suggestions: mentions,
+  };
+
+  onChange = (editorState: EditorState) => {
+    this.setState({
+      editorState,
+    });
+  };
+
+  onSearchChange = ({ value }: any) => {
+    this.setState({
+      suggestions: defaultSuggestionsFilter(value, mentions),
+    });
+  };
+
+  onAddMention = (mention: any) => {
+    console.log(`onAddMention: `, mention)
+    // get the mention object selected
+  }
+
+  focus = () => {
+    this.editor.focus();
+  };
+
   render() {
+    const { MentionSuggestions } = this.mentionPlugin;
+    const plugins = [this.mentionPlugin];
+
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+      <div className="editorContainer">
+        <div className="editor" onClick={this.focus}>
+          <Editor
+            editorState={this.state.editorState}
+            onChange={this.onChange}
+            plugins={plugins}
+            ref={(element: any) => { this.editor = element }}
+          />
+          <MentionSuggestions
+            onSearchChange={this.onSearchChange}
+            suggestions={this.state.suggestions}
+            onAddMention={this.onAddMention}
+          />
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.tsx</code> and save to reload.
-        </p>
       </div>
     );
   }
 }
-
-export default App;
