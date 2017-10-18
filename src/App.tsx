f2b2473430a7e10dda88090750d14f643db09530
Matt Mazzola <mattmazzola@live.com>
2017-10-17 21:21:33 -0700

    Try to use draft-js-mention-plugin

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,31 @@
 import * as React from 'react'
-import { Editor, EditorState, RichUtils, convertToRaw, getDefaultKeyBinding, KeyBindingUtil, DraftEditorCommand, DraftHandleValue } from 'draft-js'
+import { EditorState } from 'draft-js'
+import Editor from 'draft-js-plugins-editor'
+import createMentionPlugin, { defaultSuggestionsFilter } from 'draft-js-mention-plugin'
+import mentions, { IMention } from './mentions'
 import './App.css'
 import 'draft-js/dist/Draft.css'
-
-const { hasCommandModifier } = KeyBindingUtil
+import 'draft-js-mention-plugin/lib/plugin.css'
 
 interface Props {
 }
 
 interface State {
-  editorState: EditorState
-}
-
-const styleMap = {
-  'HIGHLIGHT': {
-    backgroundColor: 'lightgreen'
-  }
-}
-
-function keyBinding(e: React.KeyboardEvent<HTMLInputElement>): DraftEditorCommand | string | null {
-  console.log(e.key, hasCommandModifier(e))
-  if (e.key === 'ArrowUp' && hasCommandModifier(e)) {
-    return 'blis-up';
-  }
-
-  return getDefaultKeyBinding(e)
+  editorState: EditorState,
+  suggestions: IMention[]
 }
 
 class App extends React.Component<Props, State> {
   domEditor: any
-
+  mentionPlugin: any
   state = {
-    editorState: EditorState.createEmpty()
+    editorState: EditorState.createEmpty(),
+    suggestions: mentions
+  }
+  constructor(props: Props) {
+    super(props);
+
+    this.mentionPlugin = createMentionPlugin();
   }
 
   componentDidMount() {
@@ -46,45 +40,39 @@ class App extends React.Component<Props, State> {
     })
   }
 
-  onClickBold = () => {
-    this.onChange(RichUtils.toggleInlineStyle(
-      this.state.editorState,
-      'BOLD'
-    ))
+  onSearchChange = ({ value }: { value: string }) => {
+    this.setState({
+      suggestions: defaultSuggestionsFilter(value, mentions),
+    });
+  }
+
+  onAddMention = () => {
+    // get the mention object selected
   }
 
   onClickEditorContainer = () => {
     this.domEditor.focus()
   }
 
-  onClickLogRawState = () => {
-    const rawState = convertToRaw(this.state.editorState.getCurrentContent())
-    console.log(JSON.stringify(rawState, null, '  '))
-  }
-
-  handleKeyCommand = (command: string): DraftHandleValue => {
-    if (command === 'blis-up') {
-      console.log('blis-up')
-      return 'handled'
-    }
-
-    return 'not-handled'
-  }
-
   render() {
+    const { MentionSuggestions } = this.mentionPlugin
+    const plugins = [this.mentionPlugin]
+
     return (
       <div id="content">
         <h1>Draft.js Editor</h1>
-        <button onClick={this.onClickBold}>Bold</button>
-        <button onClick={this.onClickLogRawState}>Log Raw State</button>
-        <button onClick={this.onClickLogRawState}>Log Raw State</button>
+
         <div className="editor" onClick={this.onClickEditorContainer}>
           <Editor
-            customStyleMap={styleMap}
             editorState={this.state.editorState}
             onChange={this.onChange}
-            keyBindingFn={keyBinding}
-            ref={this.setDomEditorRef as any}
+            plugins={plugins}
+            ref={this.setDomEditorRef}
+          />
+          <MentionSuggestions
+            onSearchChange={this.onSearchChange}
+            suggestions={this.state.suggestions}
+            onAddMention={this.onAddMention}
           />
         </div>
       </div>
